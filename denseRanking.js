0762dfb8bbc2d8a63b80b3d6339989e1cf5d0b43
RustedMiind <74104627+RustedMiind@users.noreply.github.com>
2023-01-28 23:56:06 +0200

    deleted repeated files

--- denseRanking.js
+++ denseRanking.js
@@ -1,24 +1,14 @@
 function climbingLeaderboard(ranked, player) {
-  // Write your code here
-  const rankInDense = (array, score) => {
-    let leaderboard = [...array];
-    leaderboard.push(score);
-    console.log(leaderboard);
-    let noRepeatRanking = [];
-    for (let i in leaderboard) {
-      // console.log(leaderboard.includes(leaderboard[i]));
-      if (!noRepeatRanking.includes(leaderboard[i])) {
-        noRepeatRanking.push(leaderboard[i]);
-      }
-    }
-    noRepeatRanking.sort(function (a, b) {
-      return b - a;
-    });
-    return noRepeatRanking.indexOf(score) + 1;
-  };
-  return player.map((score) => {
-    // console.log(score, rankInDense(ranked, score));
-    return rankInDense(ranked, score);
+  return player.map((playerScore) => {
+    let leaderboard = [...ranked, playerScore];
+    return (
+      leaderboard
+        .filter((item, index) => leaderboard.indexOf(item) === index)
+        .sort(function (a, b) {
+          return b - a;
+        })
+        .indexOf(playerScore) + 1
+    );
   });
 }
 console.log(
